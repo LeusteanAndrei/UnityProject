3c5d1e5f983f2155676f6a40fa871bc256506039
Leustean Andrei <andreileustean4@gmail.com>
2025-11-07 17:19:15 +0200

    Improvements to move and jump

--- Assets/Scripts/MovementScripts/PlayerMovement/Modules/Jump.cs
+++ Assets/Scripts/MovementScripts/PlayerMovement/Modules/Jump.cs
@@ -1,56 +1,75 @@
 using UnityEditor.Search;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class Jump : MonoBehaviour
 {
 
     [SerializeField] private float jumpForce = 5f;
-    [SerializeField] private float fallMultiplier = 2.5f;
-    [SerializeField] private float upwardAcceleration = 5f;
-     
-    Rigidbody rb;
-    private bool isJumping = false;
+    [SerializeField] private float maxHoldTime = 1.0f;
+    [SerializeField] private float holdForce = 1.0f;
 
-    private void Awake()
+
+    Rigidbody rb;
+    Movement movementComponent;
+    int nrJump = 0;
+
+    float holdTime = 0;
+
+    InputAction jumpAction;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
-
-
-    private void Update()
-    {
-        if (!isJumping)
-            return;
-        //if (rb.linearVelocity.y < 0)
-        //    FallOff();
-
-        if (GetComponent<Movement>().IsGrounded())
-            isJumping = false;
+        movementComponent = GetComponent<Movement>();
+        jumpAction = InputSystem.actions.FindAction("Jump");
 
     }
 
-    public void RequestJump()
+    void Update()
     {
-        if (!GetComponent<Movement>().IsGrounded() ) // if not on ground then no can do
-            return;
-
-        if (isJumping) // if is already jumping then no can do
-            return;
-
+        if (movementComponent.GroundCheckJump())
+        {
+            nrJump = 0;
+        }
         StartJump();
     }
 
     private void StartJump()
     {
-        rb.linearVelocity = new Vector3(
-            rb.linearVelocity.x, 
-            jumpForce + Physics.gravity.y * upwardAcceleration * Time.deltaTime, 
-            rb.linearVelocity.y
-            );
-        isJumping = true;
+        if (jumpAction.WasPressedThisFrame() && nrJump < 2)
+        { 
+            RequestJump();
+            holdTime = 0f;
+        }
+        else if (jumpAction.IsPressed() && nrJump == 1 )
+        {
+            holdTime += Time.deltaTime;
+            
+            if (holdTime < maxHoldTime)
+            {
+                float t = holdTime / maxHoldTime;          
+                float smoothFactor = Mathf.SmoothStep(1f, 0f, t);
+                rb.AddForce(Vector3.up * holdForce * smoothFactor, ForceMode.Acceleration);
+            }
+        }
+        else if (jumpAction.WasReleasedThisFrame())
+        {
+            holdTime = 0f;
+        }
     }
-    private void FallOff()
+
+    public void RequestJump()
     {
-        rb.linearVelocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        nrJump += 1;
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
+
+    public bool isJumping()
+    {
+        return nrJump > 0;
+    }
+
 }
--- Assets/Scripts/MovementScripts/PlayerMovement/Modules/Movement.cs
+++ Assets/Scripts/MovementScripts/PlayerMovement/Modules/Movement.cs
@@ -1,46 +1,133 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
 
+    [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float sprintSpeed = 15f;
 
-    [Header("GroundInfo")]
-    [SerializeField] private LayerMask groundMask;
+    [Header("Ground Detection")]
+    [SerializeField] public LayerMask groundMask;
     [SerializeField] private float groundCheckDistance = 0.5f;
-    [SerializeField] private Transform groundOrigin; // the feet or bottom of the character
+    [SerializeField] public Transform groundOrigin;
+    [SerializeField] private float groundCheckJumpDistance = 0.2f;
 
-    private Vector3 movementDirection;
+    [Header("Hover Settings")]
+    [SerializeField] private float maxHoverHeight = 2f;      
+    [SerializeField] private float hoverUpwardForce = 10f;     
+    [SerializeField] private float hoverDamping = 5f;
+
     private Rigidbody rb;
     private bool isGrounded = false;
 
-    private void Awake()
+    private Vector3 movementDirection;
+    InputAction moveAction;
+    Jump jumpComponent;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
-
-    public void SetDirection(Vector3 direction)
-    {
-        movementDirection = direction;
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpComponent = GetComponent<Jump>();
     }
 
     private void Update()
     {
-        GroundCheck();
-        if (isGrounded)
+        Hover();
+        ReadInput();
+        Move();
+    }
+
+    private void RotateTowards(Vector3 vector)
+    {
+        if (vector == Vector3.zero)
+            return;
+        Quaternion toRotation = Quaternion.LookRotation(vector);
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation, 
+            toRotation, 
+            rotateSpeed * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        if (movementDirection.sqrMagnitude < 0.01f)
         {
-            Vector3 velocity = new Vector3(
-                moveSpeed * movementDirection.x,
-                rb.linearVelocity.y,
-                moveSpeed * movementDirection.z
-                );
-            rb.linearVelocity = velocity;
+            rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
+            return;
+        }
+
+        float speed = moveSpeed;
+        if (isSprinting())
+            speed = sprintSpeed;
+        RotateTowards(movementDirection);
+
+        // direct prin transform
+        //if (GroundCheck())
+        //{
+        //    transform.position += movementDirection * speed * Time.deltaTime;
+        //}
+        // cu fizica
+        Debug.Log(jumpComponent.isJumping());
+        if ( GroundCheck() || jumpComponent.isJumping() )
+        {
+            Debug.Log("aaa");
+            Vector3 desiredVelocity = movementDirection * speed;
+            Vector3 velocityChange = desiredVelocity - new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+            rb.AddForce(velocityChange, ForceMode.Acceleration);
         }
     }
 
-    void GroundCheck()
+    private bool isSprinting()
+    {
+        return Keyboard.current.leftShiftKey.isPressed;
+    }
+   
+    private void ReadInput()
+    {
+        Vector2 inputRead = moveAction.ReadValue<Vector2>();
+        movementDirection = new Vector3(inputRead.x, 0, inputRead.y).normalized;
+    }
+
+    private void Hover()
+    {
+        if(!GroundCheck())
+            return;
+        Ray ray = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit hit, maxHoverHeight * 2f, groundMask))
+        {
+            float distance = hit.distance;
+            float heightError = maxHoverHeight - distance;
+
+            float upwardSpeed = heightError * hoverUpwardForce;
+
+            float damp = -rb.linearVelocity.y * hoverDamping;
+
+            Vector3 force = Vector3.up * (upwardSpeed + damp);
+            rb.AddForce(force, ForceMode.Acceleration);
+        }
+    }
+
+    public bool GroundCheck()
     {
         isGrounded = Physics.CheckSphere(groundOrigin.position, groundCheckDistance, groundMask);
+        return isGrounded;
+    }
+
+    public bool GroundCheckJump()
+    {
+        isGrounded = Physics.CheckSphere(groundOrigin.position, groundCheckJumpDistance, groundMask);
+
+        return isGrounded;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundOrigin.position, groundCheckJumpDistance);
     }
 
     public bool IsGrounded()
