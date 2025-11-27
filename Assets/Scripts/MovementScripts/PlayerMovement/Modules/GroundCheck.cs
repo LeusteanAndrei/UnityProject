8cce410ea7d7241914b4e5ead9db3ca826b8fdc8
Leustean Andrei <andreileustean4@gmail.com>
2025-11-27 13:08:58 +0200

    Ground check fix

--- Assets/Scripts/MovementScripts/PlayerMovement/Modules/GroundCheck.cs
+++ Assets/Scripts/MovementScripts/PlayerMovement/Modules/GroundCheck.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 
@@ -5,12 +7,17 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour
 {
 
+    [SerializeField] private List<int> groundLayers = new List<int>();
+    private HashSet<int> _groundLayerSet;
+
     int groundContacts = 0; // number of ground contacts
     Movement movementComponent;
+
 
     void Start()
     {
         movementComponent = GetComponentInParent<Movement>();
+        _groundLayerSet = new HashSet<int>(groundLayers);
     }
 
 
@@ -30,14 +37,24 @@ public class GroundCheck : MonoBehaviour
         UpdateGround(other.gameObject);
     }
 
+    private void OnCollisionStay(Collision other)
+    {
+        UpdateGround(other.gameObject);
+    }
 
     private void UpdateGround(GameObject obj)
     {
         if (movementComponent == null) return;
+        if (!IsGround(obj.layer)) return;
         movementComponent.isGrounded = groundContacts > 0;
         movementComponent.currentLayerIndex = obj.layer;
     }
 
+
+    private bool IsGround(int layerNumber)
+    {
+        return _groundLayerSet.Contains(layerNumber);
+    }
 
     //// checks wether the layer of the object is the groundMask layer 
     //private bool IsGroundLayer(GameObject obj)
