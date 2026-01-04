e52d688dca7650b904ab1e3836f4b0472dd89110
Leustean Andrei <andreileustean4@gmail.com>
2026-01-04 20:29:49 +0200

    added assets

--- Assets/Scripts/SaveGameScripts/GameData.cs
+++ Assets/Scripts/SaveGameScripts/GameData.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 [System.Serializable]
@@ -6,13 +7,50 @@ public class GameData
     public string levelName;
     public float soundMeterLevel;
 
+    public float[] playerPosition;
+    public int[] brokenObjectIds;
+
     public GameData()
     {
         soundMeterLevel = 0;
         levelName = "Level 1";
+        playerPosition = null;
+        brokenObjectIds = new int[0];
     }
     public GameData(float soundMeterLevel)
     {
         this.soundMeterLevel = soundMeterLevel;
+    }
+
+
+    //public void addBrokenObjectId(int newId)
+    //{
+    //    int[] oldIds = new int[brokenObjectIds.Length + 1];
+    //    for (int i = 0; i < brokenObjectIds.Length; i++)
+    //    {
+    //        oldIds[i] = brokenObjectIds[i];
+    //    }
+    //    oldIds[oldIds.Length-1] = newId;
+    //    brokenObjectIds = oldIds;
+    //}
+
+    public void AddBrokenObjectId(int newId)
+    {
+        if (System.Array.Exists(brokenObjectIds, id => id == newId))
+        {
+            return;
+        }
+
+        int[] newArray = new int[brokenObjectIds.Length + 1];
+
+        for (int i = 0; i < brokenObjectIds.Length; i++)
+        {
+            newArray[i] = brokenObjectIds[i];
+        }
+
+        newArray[newArray.Length - 1] = newId;
+
+        brokenObjectIds = newArray;
+
     }
 }
